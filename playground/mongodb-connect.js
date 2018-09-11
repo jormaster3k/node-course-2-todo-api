c454b6d405d1339565cf6b27688ec5e00437f1c5
Osterman <josterma@UR.Rochester.edu>
2018-09-11 11:27:57 -0400

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,client) => {
+  if (err) {
+      return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  db.collection('Todos').find().toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2))
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+//   client.close();
+});
+
